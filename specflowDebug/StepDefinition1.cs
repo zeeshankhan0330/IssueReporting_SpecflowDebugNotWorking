f2104d5bef5514474135fa780b0bd6e036420035
Zeeshan.Khan <zeeshan.khan@dunnhumby.com>
2019-07-07 00:25:20 +0530

    firstCommit

---
+++ specflowDebug/StepDefinition1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TechTalk.SpecFlow;
+
+namespace specflowDebug
+{
+    [Binding]
+    public sealed class StepDefinition1
+    {
+        private Utility util;
+        public StepDefinition1(Utility util)
+        {
+            this.util = util;
+        }
+
+        [Given(@"I have entered (.*) into the calculator")]
+        public void GivenIHaveEnteredIntoTheCalculator(int p0)
+        {
+            Console.WriteLine("DebugPoint_1");
+            util.Test();
+        }
+
+    }
+}
---
+++ specflowDebug/Utility.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace specflowDebug
+{
+    public class Utility
+    {
+        public void Test()
+        {
+            Console.WriteLine("DebugPoint_2");
+        }
+    }
+}
